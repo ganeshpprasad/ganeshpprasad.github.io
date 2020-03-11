a78e089878dfc296b2744992679c26453ec3a2c8
ganeshp <ganeshpprasad@gmail.com>
2020-03-11 18:58:23 +0530

    Updates

---
+++ precache-manifest.7f335440cde35ba45eb6fe9e1434d296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bca00bc5291e8458693f2899f2e2f0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "168a4fc3a16634a80a8b",
+    "url": "/static/css/main.256f4384.chunk.css"
+  },
+  {
+    "revision": "f6a31973eb87c2a169d2",
+    "url": "/static/js/2.5b38881f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5b38881f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "168a4fc3a16634a80a8b",
+    "url": "/static/js/main.84371060.chunk.js"
+  },
+  {
+    "revision": "0c50a5f180996ee67d22",
+    "url": "/static/js/runtime-main.53a71bcf.js"
+  }
+]);
