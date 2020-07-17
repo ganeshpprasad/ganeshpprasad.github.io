43b923f7dfa3ee8d01838fd2436087f45a1bc017
ganeshpprasad <ganeshpprasad@gmail.com>
2020-07-17 14:36:10 +0530

    blog basic api addition

--- src/sections/Blogs.js
+++ src/sections/Blogs.js
@@ -1,5 +1,30 @@
 import React from 'react';
 
 export const Blogs = () => {
-	return <div>Blogs</div>;
+	const renderBlogs = (item, index) => {
+		return (
+			<div className='blog-item'>
+				<div
+					id='retainable-rss-embed'
+					data-rss='https://medium.com/feed/@ganeshpprasad'
+					data-maxcols='3'
+					data-layout='slider'
+					data-poststyle='inline'
+					data-readmore='Read the rest'
+					data-buttonclass='btn btn-primary'
+					data-offset='-100'
+				></div>
+			</div>
+		);
+	};
+
+	return (
+		<div className='blogs-con'>
+			{[
+				'React Deep End',
+				'React Shallow End',
+				'React Native Shallow',
+			].map(renderBlogs)}
+		</div>
+	);
 };
