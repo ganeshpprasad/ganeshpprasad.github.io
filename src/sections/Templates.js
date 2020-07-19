b9c590f4a9c92fe20686c4f46810b5dee9306fb7
ganeshpprasad <ganeshpprasad@gmail.com>
2020-07-19 23:41:18 +0530

    cleanup font stylilng

--- src/sections/Templates.js
+++ src/sections/Templates.js
@@ -4,18 +4,25 @@ export const Templates = () => {
 	return (
 		<div className='templ-con'>
 			<div className='templ-left'>
-				<div className='primary templ-title'>An Ecommerce template</div>
-				<div className='templ-desc'>
-					Template built using nextjs and woocomerce
+				<div className='templ-item'>
+					<div className='primary templ-title'>
+						An Ecommerce template
+					</div>
+					<div className='templ-desc'>
+						Template built using nextjs and woocomerce
+					</div>
 				</div>
+				<div className='templ-item'></div>
 			</div>
 			<div className='templ-right'>
-				<div className='primary templ-title'>
-					Food ordering service template
-				</div>
-				<div className='templ-desc'>
-					Front end template for services like food ordering with home
-					page, cart page and menu list page.
+				<div className='templ-item'>
+					<div className='primary templ-title'>
+						Food ordering service template
+					</div>
+					<div className='templ-desc'>
+						Front end template for services like food ordering with
+						home page, cart page and menu list page.
+					</div>
 				</div>
 			</div>
 		</div>
